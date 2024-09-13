9477ae3e94a0e5235333b1a2db8853b2cbcae799
Raquel <raquelbulhoes212@gmail.com>
2024-09-13 12:16:46 -0300

    adcionando banco de dados de testes

--- backend/src/Config/Conn.js
+++ backend/src/Config/Conn.js
@@ -1,11 +1,24 @@
 import {Sequelize} from "sequelize"
-import mysql from 'mysql2'
+import dotenv from "dotenv"
+import db from "./database.js"
+dotenv.config()
+
+//  const conn = new Sequelize("projeto_echoblog", "root", "Sen@iDev77!.", {
+//      host: "localhost", 
+//      dialect: "mysql"
+//  })
 
 
-const conn = new Sequelize("projeto_echoblog", "root", "Sen@iDev77!.", {
-    host: "localhost", 
-    dialect: "mysql"
-})
+
+    const conn = new Sequelize(
+        db.bd, 
+        db.user, 
+        db.password,
+        {
+        host: "localhost", 
+        dialect: "mysql"
+        }
+    )
 
 export default conn; 
 
