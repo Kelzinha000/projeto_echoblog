92dc88d01a61f7591aa2e56096b319b52f906ce4
Raquel <raquelbulhoes212@gmail.com>
2024-09-13 09:08:11 -0300

    atualizando a rota

--- backend/src/Routes/usuarioRouter.js
+++ backend/src/Routes/usuarioRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {criarUsuario, getAdm, atualizarPerfil} from "../Controller/usuarioController.js"
+import {criarUsuario, getAdm, atualizarPerfil, login} from "../Controller/usuarioController.js"
 const router = Router()
 
 router.post("/registro", criarUsuario)
 router.get("/adm", getAdm)
 router.get("/",atualizarPerfil)
+router.post("/login", login)
 
 
 export default router;
