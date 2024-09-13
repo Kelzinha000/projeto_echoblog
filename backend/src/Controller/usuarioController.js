89f65cf35a7088c01e4124a2320f972f6dceb54f
Raquel <raquelbulhoes212@gmail.com>
2024-09-13 09:04:08 -0300

    RF02-user criado

--- backend/src/Controller/usuarioController.js
+++ backend/src/Controller/usuarioController.js
@@ -2,6 +2,8 @@ import Usuarios from "../Model/usuarioModel.js";
 import conn from "../Config/Conn.js";
 import formatZodError from "../helpers/zodError.js";
 import { z } from "zod";
+import bcrypt from 'bcrypt'
+import createToken from '../helpers/zodCreateToken.js'
 
 const createShema = z.object({
   Usuario: z
@@ -92,4 +94,48 @@ export const atualizarPerfil = async (request, response) => {
     console.error(error);
     response.status(500).json({ err: "Error ao atualizar tarefa" });
   }
+};
+export const login = (request, response) => {
+ 
+  const { email, senha } = request.body;
+  if (!email) {
+    response.status(400).json({ err: "O email é obrigatório" });
+    return
+  }
+  if (!senha) {
+    response.status(400).json({ err: "A senha é obrigatória" });
+    return
+  }
+
+  const checkSql = /*sql*/ `SELECT * FROM  usuarios WHERE ?? = ?`;
+  const checkData = ["email", email];
+   conn.query(checkSql, checkData, /*async */(err, data) => {
+    if (err) {    
+      console.error(err);
+      response.status(500).json({ err: "Erro ao buscar usuario" });
+      return;
+    }
+
+    if (data.length === 0) {
+      response.status(404).json({ err: "Usuário não encontrado" });
+      return;
+    }
+     
+    const usuario = data[0];  
+    const compararSenha = /*await */bcrypt.compare(senha, usuario.senha);
+     console.log("Senha do usuário", senha)
+     console.log("Senha do objeto", usuario.senha)
+     console.log("comparar senha", compararSenha)
+    if (!compararSenha) {
+      return response.status(401).json({ message: "Senha inválida" });
+    }
+
+    try {
+      /*await*/ createToken(usuario, request, response);
+    } catch (error) {
+      response.error(error);
+      response.status(500).json({ err: "Erro ao processar informações" });
+      return
+    }
+  });
 };
